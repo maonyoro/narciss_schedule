f3d8477471fac38460835679f141b42eda052a1b
maonyoro <maonyoro@gmail.com>
2015-07-12 15:46:54 +0900

    update crawler logic and add tempfile(html)

--- batch/schedule_crawler.rb
+++ batch/schedule_crawler.rb
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+require 'date'
+
 class ScheduleCrawler < ApplicationController
   class CrawlError < StandardError; end
 
-  URL = 'http://www.zmf.co.jp/schedule/'
+  #URL = 'http://www.zmf.co.jp/schedule/'
+  URL = 'batch/201507.html'
 
   class << self
     def execute
@@ -13,17 +16,49 @@ class ScheduleCrawler < ApplicationController
     end
 
     def get_detail
+      # URLにアクセスし、得られたHTMLからパース用オブジェクトdocを生成
       html = open(URL) { |f| f.read }
       doc  = Nokogiri::HTML.parse(html, nil, 'shift-jis')
   
+      # docをパースして、スケジュールの内容をwork[]にpushする
+      work = []
       doc.xpath('//tr[@class="month"]').each do |day|
-        @schedule = Schedule.new
-        @schedule.date     = day.css('a').attribute('name').value
-        @schedule.presents = day.css('div[@class="spresents"]').inner_text
-        @schedule.title    = day.css('div[@class="stitle"]').inner_text
-        @schedule.open     = day.css('div[@class="sopen"]').inner_text
-        @schedule.band     = day.css('div[@class="sband"]').inner_text
-        @schedule.save
+        date     = day.css('a').attribute('name').value
+        presents = day.css('div[@class="spresents"]').inner_text.gsub(/[　\n]/,"")
+        title    = day.css('div[@class="stitle"]').inner_text.gsub(/[　\n]/,"")
+        open     = day.css('div[@class="sopen"]').inner_text.gsub(/[　\n]/,"")
+        band     = day.css('div[@class="sband"]').inner_text.gsub(/[　\n]/,"")
+        work.push({:date => date, :presents => presents, :title => title, :open => open, :band => band})
+      end
+      binding.pry
+
+      # HTMLから年を取得 うまく取得できなかったらDateクラスから取得する
+      /20../ =~ doc.xpath('//th[@class="month"]').inner_text
+      year = $&
+      if year.nil?
+        year = Date.today.year
+      end
+
+      work.each do |w|
+        #binding.pry
+        record = Schedule.where(:date => "#{year}#{w[:date]}")
+        if record.empty?
+          @schedule = Schedule.new
+          @schedule.date     = "#{year}#{w[:date]}"
+          @schedule.presents = w[:presents]
+          @schedule.title    = w[:title]
+          @schedule.open     = w[:open]
+          @schedule.band     = w[:band]
+          @schedule.save
+        else
+          # 既に同じdateのデータが存在する場合は更新する
+          record.update_all(
+            :presents => w[:presents],
+            :title    => w[:title],
+            :open     => w[:open],
+            :band     => w[:band]
+          )
+        end
       end
     end
 
